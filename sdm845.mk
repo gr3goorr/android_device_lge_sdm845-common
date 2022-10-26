4b932319b9e1b648174a7b19a8c164e3325684d1
Georg Veichtlbauer <georg@vware.at>
2022-10-26 19:08:30 +0200

    sdm845-common: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- sdm845.mk
+++ sdm845.mk
@@ -254,8 +254,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag \
     vendor.nxp.nxpese@1.0.vendor:64 \
     vendor.nxp.nxpnfc@1.0.vendor:64
