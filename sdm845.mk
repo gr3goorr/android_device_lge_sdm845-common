14fad3b6162e50f34458594e9a935096a29d0acd
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-19 23:28:20 +0800

    sdm845-common: Disable VINTF kernel requirements
    Change-Id: I12a75936976078a29fa0d15e24ced97d1ad9e4c1

--- sdm845.mk
+++ sdm845.mk
@@ -199,6 +199,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Kernel
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
@@ -361,9 +364,6 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
-
-# Vintf
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
 # WiFi
 PRODUCT_PACKAGES += \
