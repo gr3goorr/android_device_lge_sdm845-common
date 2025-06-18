07d0936629e3dc7c398472764fb8dc8b4cdf7276
Tuan Anh <tuan73176@gmail.com>
2025-06-18 11:01:13 +0700

    sdm845-common: Decommonize vibrator service
    - style3lm uses QTI vibrator
    
    Change-Id: Ic7b2fb3dcc02185639284b0995e5fae2b17e0a7e

--- sdm845.mk
+++ sdm845.mk
@@ -391,12 +391,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
-# Vibrator
-ifeq ($(findstring style3lm,$(DEVICE_NAME)),)
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.lge
-endif
-
 # Vintf
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
